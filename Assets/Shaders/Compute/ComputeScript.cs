48dfbf286658121bf27cc43700e5b682d3818614
Mitch93 <michele.albanese93@gmail.com>
2018-05-17 09:39:49 +0200

    Compute script example

---
+++ Assets/Shaders/Compute/ComputeScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputeScript : MonoBehaviour {
+
+    public ComputeShader shader;
+    RenderTexture tex;
+
+	void Run()
+    {
+        int kernelHandle = shader.FindKernel("CSMain");
+
+        tex = new RenderTexture(256, 256, 24);
+        tex.enableRandomWrite = true;
+        tex.Create();
+
+        shader.SetTexture(kernelHandle, "Result", tex);
+        shader.Dispatch(kernelHandle, 32, 32, 1);
+    }
+
+    void Start()
+    {
+        Run();
+
+        StartCoroutine(WaitFrame());
+
+        gameObject.GetComponent<Renderer>().material.mainTexture = tex;
+    }
+
+    private IEnumerator WaitFrame()
+    {
+        yield return new WaitForEndOfFrame();
+    }
+}
