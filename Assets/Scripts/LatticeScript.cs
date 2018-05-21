b87979be1944b0fcf6fa93223dd1020e2e47a15a
Mitch93 <michele.albanese93@gmail.com>
2018-05-21 15:56:11 +0200

    lattice WIP

--- Assets/Scripts/LatticeScript.cs
+++ Assets/Scripts/LatticeScript.cs
@@ -16,6 +16,7 @@ public class LatticeScript : MonoBehaviour {
     }
 
     // Bound values for mesh
+    Mesh mesh;
     Bounds bounds;
     Vector3 extents;
 
@@ -24,10 +25,14 @@ public class LatticeScript : MonoBehaviour {
     public IntVector3 gridParams;
     Vector3[,,] gridpoints;
 
+    // Check if mesh origin is not on center but below
+    public bool isCenterBottom = true;
+
 	// Use this for initialization
 	void Start () {
         // First of all, retrieve bounds for mesh
-        bounds = GetComponent<MeshFilter>().mesh.bounds;
+        mesh = GetComponent<MeshFilter>().mesh;
+        bounds = mesh.bounds;
         extents = bounds.extents;
 
         // Get parameters for grid
@@ -46,9 +51,47 @@ public class LatticeScript : MonoBehaviour {
                     gridpoints[i, j, k] = new Vector3 { x = (float)i / L, y = (float)j / M, z = (float)k / N };
 
         // Generate lattice vertices
-        GenerateCubes();
+        GenerateVertices();
+    }
 
+    private void Update()
+    {
+        // Check if we hit a cube with the mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                LatticeVertexScript latticeVertex = hit.transform.gameObject.GetComponent<LatticeVertexScript>();
+                if (latticeVertex != null)
+                {
+                    // Change lattice vertex and then all mesh vertices
+                    ModifyLattice(hit.transform.gameObject);   
+                }
+            }
+        }
+    }
 
+    void ModifyLattice(GameObject vertexObject)
+    {
+        // TODO: just translate for now on click
+        vertexObject.transform.position += new Vector3(0.0f, 0.2f, 0.0f);
+        var P_ijk_prime = vertexObject.transform.position;
+
+        // Get all mesh vertices and apply transformation
+        var vertices = mesh.vertices;
+
+        // For each mesh vertex...
+        for( int v = 0; v < vertices.Length; ++v )
+        {
+            // TODO
+            // 1) get STU coords
+            // 2) apply transformation to each vertex
+        }
+
+        // apply transformation
+        mesh.vertices = vertices;
     }
 
     // Transform from STU coords to local coords
@@ -56,15 +99,27 @@ public class LatticeScript : MonoBehaviour {
     {
         Vector3 res = 2 * Vector3.Scale(extents, stuCoord) - extents;
 
-        // TODO: We are assuming that all meshes will start count along the y-axis from the bottom.
-        // Be careful!
-        res.y += extents.y;
+        // Add offset if center is on the bottom of the mesh 
+        if(isCenterBottom)
+            res.y += extents.y;
+
+        return res;
+    }
+
+    // TODO Check correctness!
+    // Transform from local coords to STU coords (apply reverse transformation wrt above)
+    Vector3 GetSTUCoords(Vector3 localCoords)
+    {
+        Vector3 res;
+        res.x = (localCoords.x + extents.x) / (2.0f * extents.x);
+        res.y = (isCenterBottom) ? (localCoords.y / (2.0f * extents.y)) : (localCoords.y + extents.y) / (2.0f * extents.y);
+        res.z = (localCoords.z + extents.z) / (2.0f * extents.z);
 
         return res;
     }
 
     // Display a little cube for each vertex
-    void GenerateCubes()
+    void GenerateVertices()
     {
         // Show coordinates in STU and local systems
         for (int i = 0; i <= gridParams.L; ++i)
@@ -82,11 +137,11 @@ public class LatticeScript : MonoBehaviour {
                     cube.name = "Cube_" + i + "_" + j + "_" + k;
 
                     // Add mouse interaction script
-                    cube.AddComponent<LatticeVertex>();
+                    cube.AddComponent<LatticeVertexScript>();
 
                     // Change position and scaling
                     cube.transform.parent = transform;
-                    cube.transform.localScale *= 0.5f;
+                    cube.transform.localScale *= 0.2f;
 
                     // Set it as a child of the mesh
                     cube.transform.position = localCoords;
--- Assets/Scripts/ShaderSetupScript.cs
+++ Assets/Scripts/ShaderSetupScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ShaderSetup : MonoBehaviour {
+public class ShaderSetupScript : MonoBehaviour {
 
     Material material;
 
