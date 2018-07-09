958dc49a428a95be5ffda64e577aaea6705c676b
Mitch93 <michele.albanese93@gmail.com>
2018-07-09 11:35:47 +0200

    buttons added

---
+++ Assets/Scripts/LoadBenchmark.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadBenchmark : MonoBehaviour {
+
+    public void OnBenchmarkButton()
+    {
+        StartCoroutine(OpenBenchmark());
+    }
+
+    IEnumerator OpenBenchmark()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("BenchmarkScene");
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
